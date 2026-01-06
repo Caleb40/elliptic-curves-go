d93be7c928368ca24005b7e45121b94f1562a2fb
Caleb Olusola <calebseyi123@gmail.com>
2026-01-06 03:02:24 +0100

    feat: use bigInt to manage larger integers that might be expected during blockchain operations

--- elliptic-curve/finite-element.go
+++ elliptic-curve/finite-element.go
@@ -2,22 +2,22 @@ package elliptic_curve
 
 import (
 	"fmt"
-	"math"
+	"math/big"
 )
 
 type FieldElement struct {
 	// class FieldElement (equivalent in python)
-	order uint64 // field order
-	num   uint64 // value of the given element in the field
+	order *big.Int // field order
+	num   *big.Int // value of the given element in the field
 }
 
-func NewFieldElement(order uint64, num uint64) *FieldElement {
+func NewFieldElement(order *big.Int, num *big.Int) *FieldElement {
 	/*
 		init function for FieldElement, __init__ from python
 		(initialization)
 	*/
-	if num >= order {
-		err := fmt.Sprintf("Num not in the range of 0 to %d", order-1)
+	if order.Cmp(num) == -1 {
+		err := fmt.Sprintf("Num not in the range of 0 to %s", order.Sub(order, big.NewInt(1)))
 		panic(err)
 	}
 	return &FieldElement{
@@ -27,7 +27,7 @@ func NewFieldElement(order uint64, num uint64) *FieldElement {
 }
 
 func (f *FieldElement) checkOrder(other *FieldElement) {
-	if f.order != other.order {
+	if f.order.Cmp(other.order) != 0 {
 		panic(fmt.Sprintf(
 			"order mismatch: source element has order %d, target element has order %d",
 			f.order, other.order,
@@ -37,24 +37,26 @@ func (f *FieldElement) checkOrder(other *FieldElement) {
 
 func (f *FieldElement) String() string {
 	// equivalent of "__repr__" in python
-	return fmt.Sprintf("FieldElement {order:%d, num:%d}\n", f.order, f.num)
+	return fmt.Sprintf("FieldElement {order:%s, num:%s}\n", f.order.String(), f.num.String())
 }
 
 func (f *FieldElement) EqualTo(other *FieldElement) bool {
-	return f.order == other.order && f.num == other.num
+	return f.order.Cmp(other.order) == 0 && f.num.Cmp(other.num) == 0
 }
 
 func (f *FieldElement) Add(other *FieldElement) *FieldElement {
 	f.checkOrder(other)
 	// remember the modulo
 	// operator overloading for +, __add__ python
-	return NewFieldElement(f.order, (f.num+other.num)%f.order)
+	var op big.Int
+	return NewFieldElement(f.order, op.Mod(op.Add(f.num, other.num), f.order))
 }
 
 func (f *FieldElement) Negate() *FieldElement {
 	// (a + b) % order === order - a
 	//return NewFieldElement(f.order, (-f.num)%f.order) equivalent
-	return NewFieldElement(f.order, (f.order-f.num)%f.order)
+	var op = big.Int{}
+	return NewFieldElement(f.order, op.Sub(f.order, f.num))
 }
 
 func (f *FieldElement) Subtract(other *FieldElement) *FieldElement {
@@ -66,15 +68,20 @@ func (f *FieldElement) Subtract(other *FieldElement) *FieldElement {
 func (f *FieldElement) Multiply(other *FieldElement) *FieldElement {
 	// Arithmetic multiplication over the modulo of the order
 	f.checkOrder(other)
-	return NewFieldElement(f.order, (f.num*other.num)%f.order)
+	var op big.Int
+	mul := op.Mul(f.num, other.num)
+	return NewFieldElement(f.order, op.Mod(mul, f.order))
 }
 
-func (f *FieldElement) Power(power int64) *FieldElement {
+func (f *FieldElement) Power(power *big.Int) *FieldElement {
 	// Arithmetic power over the modulo of the order
-	return NewFieldElement(f.order,
-		uint64(math.Pow(float64(f.num), float64(power)))%f.order)
+	var op big.Int
+	powerRes := op.Exp(f.num, power, nil)
+	return NewFieldElement(f.order, op.Mod(powerRes, f.order))
 }
 
-func (f *FieldElement) ScalarMul(val uint64) *FieldElement {
-	return NewFieldElement(f.order, (f.num*val)%f.order)
+func (f *FieldElement) ScalarMul(val *big.Int) *FieldElement {
+	var op big.Int
+	mul := op.Mul(f.num, val)
+	return NewFieldElement(f.order, op.Mod(mul, f.order))
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	ecc "elliptic_curve"
 	"fmt"
+	"math/big"
 	"math/rand"
 )
 
@@ -12,17 +13,17 @@ func SolveField19MultiplySet() {
 	maximum := 18
 	k := rand.Intn(maximum-minimum) + minimum
 	fmt.Printf("Randomly selected value, k is %d\n", k)
-	element := ecc.NewFieldElement(19, uint64(k))
+	element := ecc.NewFieldElement(big.NewInt(19), big.NewInt(int64(k)))
 
 	for i := 0; i < 19; i++ {
 		fmt.Printf("Element: %d, multiplied with %d is: %v", k, i,
-			element.ScalarMul(uint64(i)))
+			element.ScalarMul(big.NewInt(int64(i))))
 	}
 }
 
 func main() {
-	f44 := ecc.NewFieldElement(57, 44)
-	f33 := ecc.NewFieldElement(57, 33)
+	f44 := ecc.NewFieldElement(big.NewInt(57), big.NewInt(44))
+	f33 := ecc.NewFieldElement(big.NewInt(57), big.NewInt(33))
 	res := f44.Add(f33)
 	fmt.Printf("Field element 44 add to field element 33 is %v", res)
 	// [-44], the negated value of 44 is 57-44 = 13 [mod 57]
@@ -31,13 +32,13 @@ func main() {
 	fmt.Printf("Field Element 44 - 33 is: %v", f44.Subtract(f33))
 	fmt.Printf("Field element 33 - 44 is %v", f33.Subtract(f44))
 	// f33 - f44 == f46 => f46 + f44 == f33 [mod 57]
-	f46 := ecc.NewFieldElement(57, 46)
+	f46 := ecc.NewFieldElement(big.NewInt(57), big.NewInt(46))
 	fmt.Printf("Field Element 46 + 44 is: %v\n", f46.Add(f44))
 
 	fmt.Printf("Product of Element 46 with itself is: %v", f46.Multiply(f46))
-	fmt.Printf("Exponent of Element 46 with 2 is: %v", f46.Power(2))
+	fmt.Printf("Exponent of Element 46 with 2 is: %v", f46.Power(big.NewInt(2)))
 	// Check:
-	fmt.Printf("CHECK: 46^2 and 46 * 46 are equivalent: %v\n\n", f46.Multiply(f46).EqualTo(f46.Power(2)))
+	fmt.Printf("CHECK: 46^2 and 46 * 46 are equivalent: %v\n\n", f46.Multiply(f46).EqualTo(f46.Power(big.NewInt(2))))
 
 	SolveField19MultiplySet()
 }
